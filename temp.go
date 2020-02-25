0a41a2396fcacc296cc574636d1bc4546b38dc10
Satinder <grewal@lavabit.com>
2020-02-26 06:19:12 +1300

    wallet temp code test

--- temp.go
+++ temp.go
@@ -16,42 +16,55 @@ type WalletInfo struct {
 	Synched bool
 }
 
-// Wallets store array of WalletInfo data type
-type Wallets struct {
-	Wallet []WalletInfo
-}
-
 func main() {
 
-	var wallets Wallets
+	var wallets []WalletInfo
 
-	for i, v := range chains {
-		// fmt.Println(i)
-		// fmt.Println(v)
-		appName := kmdgo.NewAppType(v)
+	wallets = append(wallets, WalletInfo{"KMD", "Offline", 0.3, 2, true})
+	wallets = append(wallets, WalletInfo{"PIRATE", "Online", 4.5, 56, false})
 
-		var info kmdgo.GetInfo
+	// for i, v := range chains {
+	// 	fmt.Println(i)
+	// 	fmt.Println(wallets.Wallet)
+	// 	// fmt.Println(v)
+	// 	appName := kmdgo.NewAppType(v)
 
-		info, err := appName.GetInfo()
-		fmt.Println(info)
-		if err != nil {
-			// fmt.Printf("Code: %v\n", info.Error.Code)
-			// fmt.Printf("Message: %v\n\n", info.Error.Message)
-			fmt.Println("Err happened", err)
-			wallets.Wallet[i].Status = "Offline"
-		} else {
-			fmt.Println(i, info.Result.Name)
-			// wallets.Wallet[i].Ticker = info.Result.Name
-			// wallets.Wallet[i].Status = "Online"
-			// wallets.Wallet[i].Balance = info.Result.Balance
-			// wallets.Wallet[i].Blocks = info.Result.Longestchain
-			// if info.Result.Longestchain != info.Result.Blocks {
-			// 	wallets.Wallet[i].Synched = false
-			// } else {
-			// 	wallets.Wallet[i].Synched = true
-			// }
-		}
+	// 	var info kmdgo.GetInfo
+
+	// 	info, err := appName.GetInfo()
+	// 	fmt.Println(info)
+	// 	if err != nil {
+	// 		// fmt.Printf("Code: %v\n", info.Error.Code)
+	// 		// fmt.Printf("Message: %v\n\n", info.Error.Message)
+	// 		fmt.Println("Err happened", err)
+	// 		fmt.Println("wallets.Wallet[", i, "].Status")
+	// 		// wallets.Wallet[i].Status = "Offline"
+	// 	} else {
+	// 		fmt.Println(i, info.Result.Name)
+	// 		// wallets.Wallet[i].Ticker = info.Result.Name
+	// 		fmt.Println(info.Result.Name)
+	// 		// wallets.Wallet[i].Status = "Online"
+	// 		// wallets.Wallet[i].Balance = info.Result.Balance
+	// 		// wallets.Wallet[i].Blocks = info.Result.Longestchain
+	// 		// if info.Result.Longestchain != info.Result.Blocks {
+	// 		// 	wallets.Wallet[i].Synched = false
+	// 		// } else {
+	// 		// 	wallets.Wallet[i].Synched = true
+	// 		// }
+	// 	}
+	// }
+
+	appName := kmdgo.NewAppType(`komodo`)
+
+	var info kmdgo.GetInfo
+
+	info, err := appName.GetInfo()
+	fmt.Println(info)
+
+	if err != nil {
+		fmt.Println("Err happened", err)
 	}
 
-	fmt.Println(wallets)
+	fmt.Println(len(wallets))
+	fmt.Println(wallets[0])
 }
